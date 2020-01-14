5f9b4399e0b02c41bfd11ee178213fcc9710d066
Erick <erick.hx@gmail.com>
2020-01-13 21:44:37 -0500

    (#2) (#2) feat: Implement config for custom vars .less

--- config-overrides.js
+++ config-overrides.js
@@ -1,14 +1,46 @@
-const { override, fixBabelImports, addLessLoader } = require("customize-cra");
+const {
+  override,
+  addLessLoader,
+  addWebpackAlias,
+  fixBabelImports
+} = require('customize-cra')
+
+const path = require('path')
+// const darkThemeVars = require('antd/dist/dark-theme')
+
+const resolve = dir => path.join(__dirname, '.', dir)
+const lessToJS = require("less-vars-to-js");
+const rewiredSourceMap = () => config => {
+  config.devtool = config.mode === 'development' ? 'cheap-module-source-map' : false
+  return config
+}
+const fs = require("fs");
+// Where your antd-custom.less file lives
+const themeVariables = lessToJS(
+  fs.readFileSync(
+    path.resolve(__dirname, "./assets/antd-custom.less"),
+    "utf8"
+  )
+);
 
 module.exports = override(
-  fixBabelImports("import", {
-    libraryName: "antd",
-    libraryDirectory: "es",
-    style: "css"
-  }),
-
   addLessLoader({
     javascriptEnabled: true,
-    modifyVars: { "@primary-color": "#1DA57A" }
-  })
-);
+    modifyVars: themeVariables
+
+    //  { hack: `true;@import "${require.resolve('antd/lib/style/color/colorPalette.less')}";`,
+    // ...darkThemeVars,
+    // '@primary-color': ''#13c2c2,
+    // '@dark-color': '#141414'
+    // }
+  }),
+  addWebpackAlias({
+    '~': resolve('src')
+  }),
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: true
+  }),
+  rewiredSourceMap()
+)
---
+++ src/Components/SideMenu/MenuList.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MenuList: React.FC = () => {
+  return (
+    <div>
+      <h2>Hello</h2>
+    </div>
+  );
+};
+
+export default MenuList;
